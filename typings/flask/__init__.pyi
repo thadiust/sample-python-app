df6afa2fb2ef0e6e9d4767b7ed259e96c3dad4f0
Thadius Trone <thadiust@ad.unc.edu>
2026-04-06 02:39:16 -0400

    ruff test failure

--- typings/flask/__init__.pyi
+++ typings/flask/__init__.pyi
@@ -8,9 +8,7 @@ _C = TypeVar("_C", bound=Callable[..., Any])
 class Flask:
     def __init__(self, import_name: str, **kwargs: Any) -> None: ...
     def route(self, rule: str, **options: Any) -> Callable[[_C], _C]: ...
-    def run(
-        self,
-        host: str | None = None,
+    def run(self, host: str | None = None,
         port: int | None = None,
         debug: bool | None = None,
         **options: Any,
